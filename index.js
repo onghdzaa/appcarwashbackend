f5062a72cf425d4d378fb8721f43fc5cde3227fe
Leeong <gamezzbbzz@gmail.com.com>
2021-05-15 01:06:18 +0700

    first commit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
-const PORT=process.env.port || '80'
-app.get('/', (req, res) => {
+const PORT=process.env.PORT || '80'
+app.get("/", (req, res) => {
   res.json({ message: 'Ahoy!' })
 })
 app.listen(PORT, () => {
