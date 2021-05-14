e8a7565f95cf57cec15314308fc0fd5a47049870
Leeong <gamezzbbzz@gmail.com.com>
2021-05-14 23:44:37 +0700

    first commit

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(express.json({limit: '50mb'}));
 //   origin: "http://localhost:8080",
 //   optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
 // };
-app.use(cors(corsOptions));
+//app.use(cors(corsOptions));
 app.use(bodyParser.json({limit: '50mb', extended: true}));
 app.use(bodyParser.urlencoded({ extended: true }));
 //var sess;
