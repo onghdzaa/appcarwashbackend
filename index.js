b609a29dcfbbc52f5a0ea3a5166b4ca7c086563d
Leeong <gamezzbbzz@gmail.com.com>
2021-05-15 01:23:40 +0700

    first commit

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const bodyParser = require("body-parser");
 app.use(bodyParser.json({limit: '50mb', extended: true}));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json({limit: '50mb'}));
+app.use(cors());
 app.get("/", (req, res) => {
   res.json({ message: 'Ahoy!' })
 })
