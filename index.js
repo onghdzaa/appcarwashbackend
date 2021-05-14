5ba00c9f094ee3c2794f3215c0f7356b51bda089
Leeong <gamezzbbzz@gmail.com.com>
2021-05-15 01:10:03 +0700

    first commit

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const express = require('express')
 const app = express()
 const PORT=process.env.PORT || '80'
+const cors = require("cors");
+const pool = require("./db");
+const bodyParser = require("body-parser");
+app.use(bodyParser.json({limit: '50mb', extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json({limit: '50mb'}));
 app.get("/", (req, res) => {
   res.json({ message: 'Ahoy!' })
 })
 app.listen(PORT, () => {
-  console.log('Application is running on port 9000')
+  console.log('Application is running on port '+PORT)
 })
