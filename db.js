5b6bb8b1458ff15adc9c078068c8114afa87021c
Leeong <gamezzbbzz@gmail.com.com>
2021-05-15 19:50:27 +0700

    Add existing file

--- db.js
+++ db.js
@@ -1,10 +1,10 @@
 const Pool =require("pg").Pool;
 
 const pool=new Pool({
-    user:"postgres",
-    password:"7101",
-    database:"project",
-    host:"localhost",
+    user:"loiajvqyeayzsn",
+    password:"7dbb96247113eb90337527925a1487e958e6db77157afec2df9bf323918647da",
+    database:"dc3n9diq57fl47",
+    host:"ec2-18-215-111-67.compute-1.amazonaws.com",
     port:5432
 });
 module.exports=pool;
