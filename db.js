d7136427fbca51715fd917df16f3186105ef0ff4
Leeong <gamezzbbzz@gmail.com.com>
2021-05-20 02:37:49 +0700

    add

--- db.js
+++ db.js
@@ -4,7 +4,7 @@ let pool
    // console.log("testing");
     pool = new Pool({
        
-        connectionString: "postgres://loiajvqyeayzsn:7dbb96247113eb90337527925a1487e958e6db77157afec2df9bf323918647da@ec2-18-215-111-67.compute-1.amazonaws.com:5432/dc3n9diq57fl47",
+        connectionString: "postgres://aefehyowptptaw:a860dc0e6db9bcf629d719bc20d7ce1e25cd697230eabdfd5a741d6bc83d13c3@ec2-50-17-255-120.compute-1.amazonaws.com:5432/d709omf6uc50da",
         ssl: {
             rejectUnauthorized: false
           }
